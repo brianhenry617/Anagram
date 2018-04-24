2ccf1e0b4cf5776b16a7e14cda69d69598b7dbf8
Brian Henry <brian@>
2018-04-24 09:00:01 -0700

    Update Files

---
+++ Anagram/Models/Anagram.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System;
+
+namespace Anagram.Models
+{
+  public class Anagram
+  {
+    private string _word;
+    private string _newWord;
+    private static _List<word> _instances = new List<word> {};
+
+    public Anagram (string word, string newWord)
+    {
+      _word = word;
+      _newWord = newWord;
+    }
+    public string GetWord()
+    {
+      return _word;
+    }
+    public void SetWord(string word)
+    {
+      return _newWord
+    }
+  }
+}
